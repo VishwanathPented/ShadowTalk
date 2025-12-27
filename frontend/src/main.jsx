9cd60049dd1c4821146910d88dc34f3de86e10e5
Vishwanath Pented <vishupented04@gmail.com>
2025-12-27 14:13:17 +0530

    fix: remove unused Google OAuth provider causing build error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from 'react-hot-toast'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "211140167059-iqs5gakg1i2chfh0c0phib5cl9j13eql.apps.googleusercontent.com"}>
-            <Toaster position="top-center" reverseOrder={false} />
-            <App />
-        </GoogleOAuthProvider>
+        <Toaster position="top-center" reverseOrder={false} />
+        <App />
     </React.StrictMode>,
 )
